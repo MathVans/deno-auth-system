ad99da941aab0c6db4e8f1d67fe6ee2d65271ea0
Mateus Almeida <mateusalmeida0070@gmail.com>
2025-03-03 09:03:11 -0300

    feat: add .env file and creating the docker-db

--- src/routes/protected.ts
+++ src/routes/protected.ts
@@ -1,6 +1,6 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { authMiddleware } from "../middlewares/auth.ts";
-import { getProfile } from "../controllers/auth.ts";
+import { aut } from "../controllers/auth.ts";
 import { profileRoute } from "../schemas/auth.ts";
 
 // Create a new router for protected routes
